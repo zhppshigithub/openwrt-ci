8d15a5249bb871aa08c1eb678995ca82cfb10573
ppshi <137143735+zhppshigithub@users.noreply.github.com>
2025-02-10 15:40:35 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -4,7 +4,7 @@
 # sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
-# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
